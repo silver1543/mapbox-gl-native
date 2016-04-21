ab9e25d06aaa8c37185920c6d79d176d6a6d6446
Tobrun <tobrun@mapbox.com>
2016-04-21 15:12:36 +0200

    [android] #3276 - try out Android SDK Animations

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/annotation/ViewMarkerActivity.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/annotation/ViewMarkerActivity.java
@@ -1,15 +1,21 @@
 package com.mapbox.mapboxsdk.testapp.activity.annotation;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.annotations.MarkerView;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
@@ -24,7 +30,7 @@ public class ViewMarkerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_animated_marker);
+        setContentView(R.layout.activity_marker_view);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -40,17 +46,73 @@ public class ViewMarkerActivity extends AppCompatActivity {
         mMapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull final MapboxMap mapboxMap) {
-                LatLng brussels = new LatLng(50.900446, 4.485251);
-                Marker marker = mapboxMap.addMarker(new MarkerOptions().position(brussels));
-                marker.setMarkerView(new CustomMarkerView(ViewMarkerActivity.this));
+                Marker china = mapboxMap.addMarker(new MarkerOptions().position(new LatLng(31.230416, 121.473701)));
+                final MarkerView chinaView = createMarkerView("cn", R.drawable.ic_china);
+                china.setMarkerView(chinaView);
+
+                Marker brazil = mapboxMap.addMarker(new MarkerOptions().position(new LatLng(-15.798200, -47.922363)));
+                final MarkerView brazilView = createMarkerView("br", R.drawable.ic_brazil);
+                brazil.setMarkerView(brazilView);
+
+                final Marker us = mapboxMap.addMarker(new MarkerOptions().position(new LatLng(38.907192, -77.036871)));
+                final MarkerView usView = createMarkerView("us", R.drawable.ic_us);
+                us.setMarkerView(usView);
+
+                Marker germany = mapboxMap.addMarker(new MarkerOptions().position(new LatLng(52.520007, 13.404954)));
+                final MarkerView germanyView = createMarkerView("de", R.drawable.ic_germany);
+                germany.setMarkerView(germanyView);
+
+                findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+
+                    private static final int ANIM_REPEAT_COUNT = 21;
+                    private static final int ANIM_DURATION = 750;
+
+                    @Override
+                    public void onClick(View v) {
+                        ObjectAnimator rotateAnimator = ObjectAnimator.ofFloat(germanyView, "rotation", 0f, 360f);
+                        rotateAnimator.setDuration(ANIM_DURATION);
+                        rotateAnimator.setRepeatMode(ValueAnimator.RESTART);
+                        rotateAnimator.setRepeatCount(ANIM_REPEAT_COUNT);
+                        rotateAnimator.start();
+
+                        ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(brazilView, "alpha", 0f, 1f);
+                        alphaAnimator.setDuration(ANIM_DURATION);
+                        alphaAnimator.setRepeatMode(ValueAnimator.REVERSE);
+                        alphaAnimator.setRepeatCount(ANIM_REPEAT_COUNT);
+                        alphaAnimator.start();
+
+                        ObjectAnimator xTranslateAnimator = ObjectAnimator.ofFloat(chinaView, "translationX", 12f);
+                        xTranslateAnimator.setRepeatMode(ValueAnimator.REVERSE);
+                        xTranslateAnimator.setRepeatCount(ANIM_REPEAT_COUNT);
+                        ObjectAnimator yTranslateAnimator = ObjectAnimator.ofFloat(chinaView, "translationY", 4f);
+                        yTranslateAnimator.setRepeatMode(ValueAnimator.REVERSE);
+                        yTranslateAnimator.setRepeatCount(ANIM_REPEAT_COUNT);
+                        AnimatorSet translateAnimatorSet = new AnimatorSet();
+                        translateAnimatorSet.playTogether(xTranslateAnimator, yTranslateAnimator);
+                        translateAnimatorSet.setDuration(ANIM_DURATION);
+                        translateAnimatorSet.start();
+
+                        ObjectAnimator xScaleAnimator = ObjectAnimator.ofFloat(usView, "scaleX", 4f);
+                        xScaleAnimator.setRepeatMode(ValueAnimator.REVERSE);
+                        xScaleAnimator.setRepeatCount(ANIM_REPEAT_COUNT);
+                        ObjectAnimator yScaleAnimator = ObjectAnimator.ofFloat(usView, "scaleY", 2f);
+                        yScaleAnimator.setRepeatMode(ValueAnimator.REVERSE);
+                        yScaleAnimator.setRepeatCount(ANIM_REPEAT_COUNT-5);
+                        AnimatorSet scaleAnimatorSet = new AnimatorSet();
+                        scaleAnimatorSet.playTogether(xScaleAnimator, yScaleAnimator);
+                        scaleAnimatorSet.setDuration(ANIM_DURATION);
+                        scaleAnimatorSet.start();
+                    }
+                });
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_zoom, menu);
-        return true;
+    private MarkerView createMarkerView(String countryAbbrev, @DrawableRes int countryIconRes) {
+        CustomMarkerView customMarkerView = new CustomMarkerView(this);
+        customMarkerView.setText(countryAbbrev);
+        customMarkerView.setImage(countryIconRes);
+        return customMarkerView;
     }
 
     @Override
--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/view/CustomMarkerView.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/view/CustomMarkerView.java
@@ -1,30 +1,36 @@
 package com.mapbox.mapboxsdk.testapp.view;
 
 import android.content.Context;
-import android.util.AttributeSet;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.mapbox.mapboxsdk.annotations.MarkerView;
 import com.mapbox.mapboxsdk.testapp.R;
 
-public class CustomMarkerView extends MarkerView{
+public class CustomMarkerView extends MarkerView {
+
+    private TextView textView;
+    private ImageView imageView;
 
     public CustomMarkerView(Context context) {
         super(context);
         init(context);
     }
 
-    public CustomMarkerView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init(context);
-    }
-
-    public CustomMarkerView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init(context);
-    }
-
     private void init(Context context) {
-        LayoutInflater.from(context).inflate(R.layout.view_custom_marker,this);
+        LayoutInflater.from(context).inflate(R.layout.view_custom_marker, this);
+        textView = (TextView) findViewById(R.id.textView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    public void setText(@NonNull String text) {
+        textView.setText(text);
+    }
+
+    public void setImage(@DrawableRes int drawableRes) {
+        imageView.setImageResource(drawableRes);
     }
 }
